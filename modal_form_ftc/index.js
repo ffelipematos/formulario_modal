28395cd5ca4ebd9bdc1a95e142ca28e9a9272e8f
noirtunnel <61060701+ffelipematos@users.noreply.github.com>
2020-04-14 16:53:20 -0300

    Add files via upload

--- modal_form_ftc/index.js
+++ modal_form_ftc/index.js
@@ -1,28 +1,45 @@
-function getFormData($form){
+function getFormData($form) {
     var unindexed_array = $form.serializeArray();
     var indexed_array = {};
 
-    $.map(unindexed_array, function(n, i){
+    $.map(unindexed_array, function (n, i) {
         indexed_array[n['name']] = n['value'];
     });
 
     return indexed_array;
 }
 
-$(document).ready(function(){
-    $("button").click(function(){
-      //var form = JSON.stringify($("form").serializeArray());
+$(document).ready(function () {
+    $("button").click(function () {
+        //var form = JSON.stringify($("form").serializeArray());
 
-     var form = getFormData($("form")); 
+        var form = getFormData($("form"));
 
-      $.each(form, (function(key, val){
-          console.log(key,val);
-      }))
-      
-      console.log(form);
+        $.each(form, (function (key, val) {
+            console.log(key, val);
+        }))
+
+        console.log(form);
     });
 
     $('#cpf').mask('000.000.000-00');
     $('#telefone').mask('(00) 0 0000-0000');
-    
-  });
+});
+
+function validarEmail() {
+    var email = document.querySelector('#email');
+    var error = document.querySelector('#error-email');
+
+    if (!email.checkValidity()) {
+        error.innerHTML = "Email invalido";
+    }
+}
+
+function redefinirMsg() {
+    var error = document.querySelector('#error-email');
+
+    if (error.innerHTML == "Email invalido") {
+        error.innerHTML = "";
+    }
+}
+
